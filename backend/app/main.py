248e97f7e9d52b887e3a75678b3efb7f06b96adf
xy <xxinyitoh@gmail.com>
2026-08-14 10:32:35 +0800

    wiring for render

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3003"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
